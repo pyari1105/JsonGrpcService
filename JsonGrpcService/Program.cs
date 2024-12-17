d983d330544cb836285a27490bc12501f5af142a
pyari1105 <surat11pyari@gmail.com>
2024-12-17 15:08:12 +0530

    changes

--- JsonGrpcService/Program.cs
+++ JsonGrpcService/Program.cs
@@ -47,6 +47,6 @@ app.UseSwaggerUI(c =>
 // Configure the HTTP request pipeline.
 app.MapGrpcService<EmployeeDetailService>();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
 
 app.Run();
